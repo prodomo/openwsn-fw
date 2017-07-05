5727ed81e2065fb05c16fac5c1b4e5532249d6e1
prodomo <nprodomo@gmail.com>
2017-07-05 10:54:24 +0800

    check can receive packet in db successfully

--- openapps/usaki/usaki.py
+++ openapps/usaki/usaki.py
@@ -1,6 +1,6 @@
 import socket
 import struct
-from datetime import datetime
+import datetime
 import MySQLdb
 
 CONST = 0.58134
@@ -12,6 +12,51 @@ OFFSET_0C = OFFSET_DATASHEET_25C - (25 * TEMP_COEFF)
 socket_handler = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
 socket_handler.bind(('',2424))
 
+def convert_adc_to_batt_by_id(in_ID, in_value):
+    matrix2 = {'a732':42, 'a720':-24, 'a6f4':0, 'a714':8, 'a636':6, 'a739':26, 'a6ab':10, 'a668':13, 'a6b2':0, 'a6fa':28, 'a6e4':-12}
+
+    parm_global_carib_batt = 0.9925
+    parm_digi2volt=3.3/2047.0
+    parm_digi2volt *= parm_global_carib_batt
+    #parm_ADCvolt2BATTvolt = 1/0.4498
+    parm_ADCvolt2BATTvolt = 1/0.5035
+
+    value = matrix2.get(in_ID)
+
+    print 'offset={0} for {1}'.format(value, in_ID)
+
+    if (value == None ):
+        offset = 0
+    else:
+        offset = value
+
+    temp_volt = (in_value + offset)
+    tmp_real = temp_volt * parm_digi2volt
+    tmp_real2 = tmp_real * parm_ADCvolt2BATTvolt
+
+    print 'external pure Battery ADC value = {0}, estimated Volt={1}V, estimated BattV={2}'.format(in_value, tmp_real, tmp_real2)
+
+    return tmp_real2
+
+def convert_adc_to_temp_by_id(in_ID, in_value):
+    matrix = {'a732':42, 'a720':-24, 'a6f4':0, 'a714':8, 'a636':6, 'a739':26, 'a6ab':10, 'a668':13, 'a692':2, 'a6fa':28}
+
+    value = matrix.get(in_ID)
+
+    print 'offset={0} for {1}'.format(value, in_ID)
+
+    if (value == None ):
+        offset = 0
+    else:
+        offset = value
+
+    temp_volt = (in_value + offset) / 615.80
+    tmp_real = (in_value + offset) *0.069 - 20.09
+
+    print 'external pure temp ADC value = {0}, estimated Volt={1}V, estimated temp={2}'.format(in_value, temp_volt, tmp_real)
+
+    return tmp_real
+
 while True:
     
     # wait for a request
@@ -20,69 +65,80 @@ while True:
  
     hisAddress     = dist_addr[0]
     hisPort        = dist_addr[1]
+    systemTime = datetime.datetime.now()
 
 #    counter        = struct.unpack('<h',data)[0]
-    counter,data1,data2,data3,data4,data5 = struct.unpack('HHHHHH',data)
+    counter,int_temp,ext_temp,ext_pyra,int_volt,gpio_pulse = struct.unpack('HHHHHH',data)
     
-    print 'len=%d, counter=%x, d1=%x, d2=%x, d3=%d, d4=%x' % (len(data),counter,data1,data2,data3,data4)
     #d1, inner temp
     #d2, exter temp
     #d3, pyra
     #d4, volt
-    pure_value = data1
-    temp_volt = pure_value * CONST
-    i_temp_real = (temp_volt - OFFSET_0C) / TEMP_COEFF
+    temp_volt = int_temp * CONST
+    #i_temp_real = (temp_volt - OFFSET_0C) / TEMP_COEFF
+    i_temp_real = int_temp
     print 'internal temp = %2.2f' % i_temp_real
 
-
-    pure_value = data2
-    temp_volt = pure_value * 1200 / 2048
-    temp_real = (temp_volt - 0.1678) / 12.223
+    hisAddress_split = hisAddress.split(':')
+    #pure_value = ext_temp
+    temp_real = convert_adc_to_temp_by_id(hisAddress_split[5], ext_temp)
+    #temp_volt = pure_value / 615.80
+    #temp_real = pure_value *0.069 - 20.09
     print 'external temp = %2.2f' % temp_real
-    
-    pure_value = data3
-    if (pure_value > 2047):
+
+    #pure_value = ext_pyra
+    if (ext_pyra > 2047):
         pyra_real = 0
         print 'detect estimated pyra < 0'
+        pyra_raw = ext_pyra
     else:
-        pyra_volt = pure_value * 1200 / 2048
-        pyra_real = pyra_volt * 1000 / 1200
-        print 'estimated pyra = %d' % pyra_real    
+        #pyra_volt = pure_value * 1200 / 2048
+        #pyra_real = pyra_volt * 1000 / 1200
+        pyra_raw = ext_pyra
+        pyra_real = convert_adc_to_batt_by_id(hisAddress_split[5], ext_pyra)
+        print 'PA0={0}, estimated pyra = {1}'.format(ext_pyra, pyra_real)
+    print 'external batt = %2.2f V' % pyra_real
 
-    pure_value = data4
-    i_volt_real = pure_value / 364.5
-    print 'raw i_volt = %2.2f' % i_volt_real
-   
-    hisAddress_split = hisAddress.split(':')
-    address = ":".join(hisAddress_split[2:])
-    #print 'hisAddress=%s %s %s %s'%(hisAddress_split[2],hisAddress_split[3],hisAddress_split[4],hisAddress_split[5])
+    #pure_value = int_volt
+    #i_volt_real = pure_value / 586.85
+    i_volt_real = int_volt
+    #print 'raw i_volt = %2.2f' % i_volt_real
+    print 'raw i_volt = %d' % i_volt_real
+
+    address = ''
+    for i in hisAddress_split[2:]:
+        address += '%04x' % int(i, 16)
+    
+    print 'hisAddress=%s %s %s %s'%(hisAddress_split[2],hisAddress_split[3],hisAddress_split[4],hisAddress_split[5])
 
     #open db connection 
     db = MySQLdb.connect("localhost","root","sakimaru","ITRI_OpenWSN" )
     cursor = db.cursor()
 
     #get current time
-    t = datetime.now()
-    currtime = t.strftime("%Y-%m-%d %H:%M:%S")
+    #t = datetime.datetime.now()
+    currtime = systemTime.strftime("%Y-%m-%d %H:%M:%S")
 
     #mySql cmd
-    sql = "INSERT INTO itri_MOEA_sensor(serialNumber, mac_addr, \
-		ext_temperature, pyranometer, datetime, int_temperature, battery_volt) \
-		VALUES ('%d', '%s', '%.2f', '%d', '%s', '%.2f', '%.2f')" %\
-		(counter, address, temp_real, pyra_real, currtime, i_temp_real, i_volt_real)
+    sql = "INSERT INTO itri_MOEA_sensor(sn, mac_addr, \
+        ext_temperature, pyranometer, datetime, int_temperature, battery_volt) \
+        VALUES ('%d', '%s', '%.2f', '%d', '%s', '%.2f', '%.2f')" %\
+        (counter, address, temp_real, pyra_raw, currtime, i_temp_real, pyra_real)
+    rps_sql = "REPLACE INTO itri_MOEA_current_sensor(sn, mac_addr, \
+        ext_temperature, pyranometer, datetime, int_temperature, battery_volt) \
+        VALUES ('%d', '%s', '%.2f', '%d', '%s', '%.2f', '%.2f')" %\
+        (counter, address, temp_real, pyra_raw, currtime, i_temp_real, pyra_real)
     try:
         # Execute the SQL command
         cursor.execute(sql)
+        cursor.execute(rps_sql)
         # Commit your changes in the database
         db.commit()
     except:
         # Rollback in case there is any error
         db.rollback()
-	
+    
     # disconnect from server
     db.close()
 
     print 'insert DB ok !!'
-
-    print 'received from [{0}]:{1} \
-    '.format(hisAddress,hisPort)
